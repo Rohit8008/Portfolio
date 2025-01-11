652715e2e2ea94e7971b394e380489c7573bc1c1
rohit8008 <rohitmittal137@gmail.com>
2025-01-11 16:44:53 +0530

    client review section complete

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Hero from './Hero/Hero'
-import About from './About/About'
-import Services from './Services/Services'
-import Projects from './Projects/Projects'
-import Skills from './Skills/Skills'
+import React from "react";
+import Hero from "./Hero/Hero";
+import About from "./About/About";
+import Services from "./Services/Services";
+import Projects from "./Projects/Projects";
+import Skills from "./Skills/Skills";
+import Reviews from "./Reviews/Reviews";
 
 function Home() {
   return (
-    <div className='overflow-hidden'>
-      <Hero/>
-      <About/>
-      <Services/>
-      <Projects/>
-      <Skills/>
+    <div className="overflow-hidden">
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Skills />
+      <Reviews />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
