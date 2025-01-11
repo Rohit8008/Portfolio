8b6ac0bd3cd9bc79f9640d1bd06201d76f681bd3
rohit8008 <rohitmittal137@gmail.com>
2025-01-11 15:07:25 +0530

    completed projects and skills section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from './Hero/Hero'
 import About from './About/About'
 import Services from './Services/Services'
+import Projects from './Projects/Projects'
+import Skills from './Skills/Skills'
 
 function Home() {
   return (
@@ -9,6 +11,8 @@ function Home() {
       <Hero/>
       <About/>
       <Services/>
+      <Projects/>
+      <Skills/>
     </div>
   )
 }
